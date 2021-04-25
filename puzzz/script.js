ef014e689a3bac4da73e3c78ba4faa09fcfd7089
explorethetime <83158463+explorethetime@users.noreply.github.com>
2021-04-25 15:48:10 +0300

    Add files via upload

---
+++ puzzz/script.js
@@ -0,0 +1,107 @@
+var img = ['https://s0.rbk.ru/v6_top_pics/media/img/5/45/756115367772455.jpg', 'https://www.ok-magazine.ru/images/cache/2021/3/19/fit_795_547_false_crop_5472_3076_0_76_q90_763741_0426f3d87cfb405c8f458ed87.jpeg', 'https://s6.cdn.teleprogramma.pro/wp-content/uploads/2020/09/aaef873e500ea50167bba4a6e3b09472.jpg', 'https://ont.by/static/article/big/index/ms65Xx8YObxYYOwD.jpg', 'https://www.film.ru/sites/default/files/filefield_paths/scarjo.jpg', 'https://static.kinoafisha.info/k/news/1200/upload/news/189183427790.jpg.webp'];
+var old = 5;
+var clicks = 0;
+function randomize() {
+  let root = document.documentElement
+  root.style.setProperty('--image','url('+img[old]+')')
+  old++
+  if(old > 5) {
+    old = 0
+  }  
+  var ul = document.querySelectorAll('#puzz i');
+  for(var i=0;i<ul.length;i++){
+    ul[i].style.left = Math.random()*(window.innerWidth-100) + 'px'
+    ul[i].style.top = Math.random()*(window.innerHeight-100) + 'px'
+  }
+  // for (var i = ul.children.length; i >= 0; i--) {
+  //   ul.appendChild(ul.children[Math.random() * i | 0]);    
+  // }
+}
+randomize()
+
+function reload() {
+  var done = document.querySelectorAll('.done')
+  done.forEach(function(e){
+    e.classList.toggle('done')
+  })
+  var dropped = document.querySelectorAll('.dropped')
+  dropped.forEach(function(e){
+    e.classList.toggle('dropped')
+  })
+  var allDone = document.querySelector('.allDone')
+  allDone.style = ''
+  allDone.classList.toggle('allDone')
+}
+
+
+// mobile functionality
+var p = document.querySelectorAll('#puzz i')
+p.forEach(function(e){
+  e.addEventListener('mousedown', function(){
+    clicks++
+    document.querySelector('#clicks').innerHTML = clicks
+  })
+  e.addEventListener('click', function(){
+    if(document.querySelector('.clicked')){
+      document.querySelector('.clicked').classList.toggle('clicked')
+      e.classList.toggle('clicked')
+    } else {
+      e.classList.toggle('clicked')  
+    }    
+  })
+})
+
+var fp = document.querySelectorAll('#puz i')
+fp.forEach(function(el){
+  el.addEventListener('click', function(){
+    if(document.querySelector('.clicked')){
+      var c = document.querySelector('.clicked')
+      if(c.classList.contains(el.classList)) {
+        el.classList.add('dropped')
+        c.classList.add('done')
+        c.classList.toggle('clicked')
+
+        if(document.querySelectorAll('.dropped').length == 9) {
+          document.querySelector('#puz').classList.add('allDone')
+          document.querySelector('#puz').style.border = 'none'  
+          document.querySelector('#puz').style.animation = 'allDone 1s linear forwards'
+
+          setTimeout(function(){
+            reload()
+            randomize()            
+          },1500)
+        } 
+      }
+    }    
+  })
+})
+
+// desktop drag and drop
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.className);  
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text")
+
+  if(ev.target.className == data){
+    ev.target.classList.add('dropped')
+    document.querySelector('.'+data+"[draggable='true']").classList.add('done')
+
+    if(document.querySelectorAll('.dropped').length == 9) {
+      document.querySelector('#puz').classList.add('allDone')
+      document.querySelector('#puz').style.border = 'none'  
+      document.querySelector('#puz').style.animation = 'allDone 1s linear forwards'  
+
+      setTimeout(function(){
+        reload()
+        randomize()        
+      },1500)
+    }    
+  }  
+}
