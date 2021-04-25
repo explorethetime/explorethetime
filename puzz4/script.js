b3b93b23a981a2c7c4fca4ae33234981d9d803ea
explorethetime <83158463+explorethetime@users.noreply.github.com>
2021-04-25 15:52:57 +0300

    Add files via upload

---
+++ puzz4/script.js
@@ -0,0 +1,108 @@
+
+var img = ['https://wl-adme.cf.tsp.li/resize/728x/jpg/b61/d0f/8923155db3b6f2fd6a84cd05a6.jpg', 'https://beztabu.net/uploads/770x433_DIR/media_news/2018/12/5c1bbb6dc9949090800340.jpg', 'https://kinowar.com/wp-content/uploads/2020/05/%D0%98%D0%BD%D1%82%D0%B5%D1%80%D1%81%D1%82%D0%B5%D0%BB%D0%BB%D0%B0%D1%80-Interstellar.jpg', 'https://ic.pics.livejournal.com/ego_drakonis/84615623/21529/21529_800.jpg', 'https://www.film.ru/sites/default/files/images/1(10).jpg', 'https://img.tyt.by/n/05/f/titanik_.jpg'];
+var old = 5;
+var clicks = 0;
+function randomize() {
+  let root = document.documentElement
+  root.style.setProperty('--image','url('+img[old]+')')
+  old++
+  if(old > 5) {
+    old = 0
+  }  
+  var ul = document.querySelectorAll('#puzz i');
+  for(var i=0;i<ul.length;i++){
+    ul[i].style.left = Math.random()*(window.innerWidth-100) + 'px'
+    ul[i].style.top = Math.random()*(window.innerHeight-100) + 'px'
+  }
+  // for (var i = ul.children.length; i >= 0; i--) {
+  //   ul.appendChild(ul.children[Math.random() * i | 0]);    
+  // }
+}
+randomize()
+
+function reload() {
+  var done = document.querySelectorAll('.done')
+  done.forEach(function(e){
+    e.classList.toggle('done')
+  })
+  var dropped = document.querySelectorAll('.dropped')
+  dropped.forEach(function(e){
+    e.classList.toggle('dropped')
+  })
+  var allDone = document.querySelector('.allDone')
+  allDone.style = ''
+  allDone.classList.toggle('allDone')
+}
+
+
+// mobile functionality
+var p = document.querySelectorAll('#puzz i')
+p.forEach(function(e){
+  e.addEventListener('mousedown', function(){
+    clicks++
+    document.querySelector('#clicks').innerHTML = clicks
+  })
+  e.addEventListener('click', function(){
+    if(document.querySelector('.clicked')){
+      document.querySelector('.clicked').classList.toggle('clicked')
+      e.classList.toggle('clicked')
+    } else {
+      e.classList.toggle('clicked')  
+    }    
+  })
+})
+
+var fp = document.querySelectorAll('#puz i')
+fp.forEach(function(el){
+  el.addEventListener('click', function(){
+    if(document.querySelector('.clicked')){
+      var c = document.querySelector('.clicked')
+      if(c.classList.contains(el.classList)) {
+        el.classList.add('dropped')
+        c.classList.add('done')
+        c.classList.toggle('clicked')
+
+        if(document.querySelectorAll('.dropped').length == 9) {
+          document.querySelector('#puz').classList.add('allDone')
+          document.querySelector('#puz').style.border = 'none'  
+          document.querySelector('#puz').style.animation = 'allDone 1s linear forwards'
+
+          setTimeout(function(){
+            reload()
+            randomize()            
+          },1500)
+        } 
+      }
+    }    
+  })
+})
+
+// desktop drag and drop
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.className);  
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text")
+
+  if(ev.target.className == data){
+    ev.target.classList.add('dropped')
+    document.querySelector('.'+data+"[draggable='true']").classList.add('done')
+
+    if(document.querySelectorAll('.dropped').length == 9) {
+      document.querySelector('#puz').classList.add('allDone')
+      document.querySelector('#puz').style.border = 'none'  
+      document.querySelector('#puz').style.animation = 'allDone 1s linear forwards'  
+
+      setTimeout(function(){
+        reload()
+        randomize()        
+      },1500)
+    }    
+  }  
+}
